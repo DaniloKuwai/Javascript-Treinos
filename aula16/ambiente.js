a443d23d6edfb5da00d184025a750a8c31e230ef
DaniloKuwai <87211978+DaniloKuwai@users.noreply.github.com>
2022-04-27 20:24:55 -0300

    aula 16 - Estrutura de repetição
    Nessa aula vamos ver o while(estrutura de repetição no ínicio) e o do while(estrutura de repetição no final)

---
+++ aula16/ambiente.js
@@ -0,0 +1,17 @@
+//Temos essa estrutura de repetição no ínicio
+
+var c = 1
+while(c < 7){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+
+//Temos essa estrutura de repetição no final que é o do while
+
+var d = 1
+do{
+    console.log(`Essa etapa é do ínicio, estamos na etapa ${d}`)
+    d++
+}while(d < 4)
+
