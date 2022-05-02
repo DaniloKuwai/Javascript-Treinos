565c55fd1e96198f4abb51539b877889069942ba
Danilo Kuwai <danilorkuwai@gmail.com>
2022-05-02 20:35:13 -0300

    aula18-exercicio-concluido

--- aula18_exercicios/js/script.js
+++ aula18_exercicios/js/script.js
@@ -5,18 +5,30 @@ var fim = Number(document.getElementById('txtfim').value)
 var qtd = Number(document.getElementById('txtqtd').value)
 var txt = document.getElementById('msg')
 
-while(inicio <= fim){ 
-    inicio++
-    console.log('Ola')
+
+
+        if(inicio <= 0 ||fim <= 0 || qtd <=0){
+            txt.innerHTML = 'Dados inválidos verifique o campo fim ou o campo passo não podem estar em branco'
+        }
+        else if(inicio < fim){
+            for(var x = inicio; x < fim ;x+=qtd){
+            resultado = inicio += qtd
+            txt.innerHTML += ` ${resultado} \u{1f449} `
+                    }        
+                    
+            }else if(inicio > fim){
+                for(var x = inicio; x > fim; x-=qtd){
+                    resultado = inicio -= qtd 
+                    txt.innerHTML += ` ${resultado} \u{1f449} `
+                }
+                
+            }
 }
 
 /*
-for(var x = inicio;x<=fim;x++){
-        var control = inicio
-        var resultado = control + qtd
-        //txt.innerHTML = 'Olá'
-        console.log(resultado)
-        inicio++
-    }
- */   
+while(inicio < fim){ 
+    console.log(inicio)
+    resultado = inicio += qtd
+    txt.innerHTML += ` ${resultado} ` 
 }
+*/
